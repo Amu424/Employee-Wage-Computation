9668030ad6c28454e4d82b968f8678b62d6712ec
softtech <amols424@gmail.com>
2021-01-12 13:34:10 +0530

    wages for month

--- Employee-Wage-Computation/empWageComputation.sh
+++ Employee-Wage-Computation/empWageComputation.sh
@@ -1,20 +1,31 @@
 #!/bin/bash -x
-empCheck=$(( RANDOM%3 ))
-IS_FULL_TIME=1
-IS_PART_TIME=2
+IS_FULLTIME=1
+IS_PARTTIME=2
 EMP_RATE_PER_HR=20
 FULL_TIME=8
 PART_TIME=4
 #variable
+numOfWorkingDays=20
 dailyWage=0
-if [ $empCheck -eq 1 ]
-  then
-    echo "employee present"
-dailyWage=$(( $EMP_RATE_PER_HR* $FULL_TIME ))
- elif [ $empCheck -eq 2 ]
-then
-    echo "employee is part time"
-    dailyWage=$(( $EMP_RATE_PER_HR* $PART_TIME))
-	else
-echo "employee is absent"
-fi
+empHrs=0
+monthlyWage=0
+for (( day=1;day<= $numOfWorkingDays;day++ ))
+do
+empCheck=$(( RANDOM%3 ))
+ case $empCheck in
+			$IS_FULLTME )
+  				empHrs=8
+			echo "Employee is full time"
+					;;
+			$IS_PARTTIME )
+				empHrs=4
+				echo "employee is part time"
+						;;
+			* )
+				empHrs=0
+			echo "Employee is absent"
+				;;
+esac
+dailyWage=$(( $empHrs* 	$EMP_RATE_PER_HR ))
+monthlyWage=$(( $monthlyWage + $dailyWage ))
+ done
