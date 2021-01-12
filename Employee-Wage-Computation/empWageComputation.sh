d82c030790f4d13c71261a80004c7d7537034cd6
softtech <amols424@gmail.com>
2021-01-12 14:04:20 +0530

    Added cal the wags till condition is met

--- Employee-Wage-Computation/empWageComputation.sh
+++ Employee-Wage-Computation/empWageComputation.sh
@@ -4,13 +4,16 @@ IS_PARTTIME=2
 EMP_RATE_PER_HR=20
 FULL_TIME=8
 PART_TIME=4
+MAX_HRS_IN_MONTH=100
+TOTAL_WORKING_DAYS=20
 #variable
-numOfWorkingDays=20
+numOfWorkingDays=0
 dailyWage=0
 empHrs=0
 monthlyWage=0
-for (( day=1;day<= $numOfWorkingDays;day++ ))
+while [[ $empHrs -lt $MAX_HRS_IN_MONTH && $numOfWorkingDays -lt $TOTAL_WORKING_DAYS ]]
 do
+((numOfWorkingDays++))
 empCheck=$(( RANDOM%3 ))
  case $empCheck in
 			$IS_FULLTME )
@@ -27,5 +30,6 @@ empCheck=$(( RANDOM%3 ))
 				;;
 esac
 dailyWage=$(( $empHrs* 	$EMP_RATE_PER_HR ))
+
 monthlyWage=$(( $monthlyWage + $dailyWage ))
- done
+done
