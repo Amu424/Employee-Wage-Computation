4e9ed6fd4ed07962dfb70e4786e7e466c5524a22
softtech <amols424@gmail.com>
2021-01-12 11:43:37 +0530

    Added using switch case

--- Employee-Wage-Computation/empWageComputation.sh
+++ Employee-Wage-Computation/empWageComputation.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -x
-empCheck=$(( RANDOM%2 ))
-IS_PRESENT=1
+empCheck=$(( RANDOM%3 ))
+IS_FULLTIME=1
+IS_PARTTIME=2
 EMP_RATE_PER_HR=20
 FULL_TIME=8
+PART_TIME=4
 #variable
 dailyWage=0
-if [ $empCheck -eq 1 ]
-  then
-    echo "employee present"
-dailyWage=$(( $EMP_RATE_PER_HR* $FULLTIME ))
- else
-    echo "employee is absent"
-fi
+empHrs=0
+ case $empCheck in
+			$IS_FULLTME )
+  				empHrs=8
+			echo "Employee is full time"
+					;;
+			$IS_PARTTIME )
+				empHrs=4
+				echo "employee is part time"
+						;;
+			* )
+				empHrs=0
+			echo "Employee is absent"
+				;;
+esac
+dailyWage=$(( $empHrs* 	$EMP_RATE_PER_HR ))
+echo $dailyWage
